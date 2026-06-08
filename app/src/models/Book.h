28dd03c4ab474f449d075ccc23aca4218f6cc7dc
Toumani-k <toumanikanoute250@gmail.com>
2026-06-08 23:20:11 +0000

    implementation de la classe livre (Book)

--- app/src/models/Book.h
+++ app/src/models/Book.h
@@ -1,4 +1,3 @@
-// Book.h - Définition de la classe Book
 #ifndef BOOK_H
 #define BOOK_H
 
@@ -6,15 +5,30 @@
 
 class Book {
 private:
-    std::string title;
-    std::string author;
-    int year;
+    std::string titre;
+    std::string auteur;
+    std::string isbn;
+    bool disponible;
 
 public:
-    Book(const std::string& title, const std::string& author, int year);
-    std::string getTitle() const;
-    std::string getAuthor() const;
-    int getYear() const;
+    // Constructeurs
+    Book();
+    Book(std::string titre, std::string auteur, std::string isbn);
+
+    // Getters
+    std::string getTitre() const;
+    std::string getAuteur() const;
+    std::string getIsbn() const;
+    bool estDisponible() const;
+
+    // Setters
+    void setTitre(const std::string& titre);
+    void setAuteur(const std::string& auteur);
+    void setIsbn(const std::string& isbn);
+    void setDisponible(bool disponible);
+
+    // Méthodes
+    void afficher() const;
 };
 
 #endif
